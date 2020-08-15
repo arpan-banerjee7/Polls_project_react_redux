3cb1ccba758dfc6f47037d15afea73ae88b06dca
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-08-15 11:04:19 +0530

    shraed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import * as React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 
 export default function App() {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.authedUser === null);
+
+  React.useEffect(() => {
+    dispatch(handleInitialData());
+  }, [dispatch]);
+
   const store = useSelector((store) => store);
   console.log("Store", store);
 
-  return <div>Redux Polls</div>;
+  return (
+    <div className="container">
+      {loading === true ? null : <div>Redux Polls</div>}
+    </div>
+  );
 }
